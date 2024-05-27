a718c6bcdae09dacc59439cab120fdedc302a09a
MdTamimAhamed <tamimssgts999@gmail.com>
2024-05-27 23:31:10 +0600

    small changes

--- src/components/ExtraSection/SocialProfile.jsx
+++ src/components/ExtraSection/SocialProfile.jsx
@@ -29,27 +29,27 @@ function SocialProfile() {
   ];
   return (
     <section className="mt-10 text-xl font-bold p-8 border-2 border-slate-100 rounded-2xl">
-      <div>
-        <div className="flex flex-wrap lg:flex-nowrap items-center justify-center gap-6 text-3xl dark:text-darkMode-textGray md:gap-4 md:text-[26px]">
-          {socialData.map((item) => (
-            <div className=" group relative z-10 basis-1/4 flex items-center border-slate-100 border-2 p-2 rounded-full">
-              <figure className="bg-black w-12 h-12 flex items-center justify-center rounded-full">
-                <a
-                  className="cursor-pointer transition-all duration-150 text-white ease-in hover:text-primary"
-                  href={item.address}
-                  target="_blank"
-                >
-                  {item.icon}
-                </a>
-              </figure>
-              <p className="ml-6 font-medium text-lg">{item.text}</p>
-              <span
-                className="absolute inset-0 -z-10 before:block before:w-full before:h-full before:bg-red before:rounded-full top-1/2 
-              -translate-y-1/2 "
-              ></span>
-            </div>
-          ))}
-        </div>
+      <div className="flex flex-wrap lg:flex-nowrap items-center justify-center gap-6 text-3xl dark:text-darkMode-textGray md:gap-4 md:text-[26px]">
+        {socialData.map((item) => (
+          <div className=" group relative overflow-hidden z-10 basis-1/4 flex items-center border-slate-100 border-2 p-2 rounded-full">
+            <figure className="bg-black w-10 h-10 flex items-center justify-center rounded-full">
+              <a
+                className="cursor-pointer transition-all duration-150 text-white ease-in hover:text-primary"
+                href={item.address}
+                target="_blank"
+              >
+                {item.icon}
+              </a>
+            </figure>
+            <p className="ml-6 font-medium text-lg group-hover:text-white">
+              {item.text}
+            </p>
+            <span
+              className="absolute inset-0 -z-10 before:block before:w-0 before:h-full before:bg-black 
+              before:rounded-full   group-hover:before:w-full before:transition-all before:duration-300 before:ease-in"
+            ></span>
+          </div>
+        ))}
       </div>
     </section>
   );
